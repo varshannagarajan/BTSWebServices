d37a48d23c301d808bea3fb2bc6c0fd1e00fe45d
Varshan Nagarajan <vnagarajan1@myseneca.ca>
2019-09-19 17:22:28 -0400

    formated manger and server

--- Web Services/manager.js
+++ Web Services/manager.js
@@ -1,30 +1,28 @@
 // Setup
-const mongoose = require('mongoose');
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
+const mongoose = require("mongoose");
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
 
 // Load the schemas
-const eventSchema = require('./Schemas/sch-event.js');
-const userSchema = require('./Schemas/sch-user.js');
-const companySchema = require('./Schemas/sch-company.js');
+const eventSchema = require("./Schemas/sch-event.js");
+const userSchema = require("./Schemas/sch-user.js");
+const companySchema = require("./Schemas/sch-company.js");
 
-module.exports = function (mongoDBConnectionString) {
-
+module.exports = function(mongoDBConnectionString) {
   let Event; // defined on connection to the new db instance
   let User;
   let Company;
 
   return {
-
-    connect: function () {
-      return new Promise(function (resolve, reject) {
+    connect: function() {
+      return new Promise(function(resolve, reject) {
         let db = mongoose.createConnection(mongoDBConnectionString);
 
-        db.on('error', (error) => {
+        db.on("error", error => {
           reject(error);
         });
 
-        db.once('open', () => {
+        db.once("open", () => {
           Event = db.model("event", eventSchema, "event");
           User = db.model("user", userSchema, "user");
           Company = db.model("company", companySchema, "company");
@@ -35,12 +33,11 @@ module.exports = function (mongoDBConnectionString) {
     },
 
     /*******************************************************          EVENTS         *********************************************************************/
-    eventsGetAll: function () {
-      return new Promise(function (resolve, reject) {
-
+    eventsGetAll: function() {
+      return new Promise(function(resolve, reject) {
         // Fetch all documents
         Event.find()
-          .sort({ eventId: 'asc' })
+          .sort({ eventId: "asc" })
           .exec((error, items) => {
             if (error) {
               // Query error
@@ -49,13 +46,11 @@ module.exports = function (mongoDBConnectionString) {
             // Found, a collection will be returned
             return resolve(items);
           });
-      })
+      });
     },
 
-
-    eventsGetById: function (eventId) {
-      return new Promise(function (resolve, reject) {
-
+    eventsGetById: function(eventId) {
+      return new Promise(function(resolve, reject) {
         // Find one specific document
         Event.findById(eventId, (error, item) => {
           if (error) {
@@ -67,15 +62,14 @@ module.exports = function (mongoDBConnectionString) {
             // Found, one object will be returned
             return resolve(item);
           } else {
-            return reject('Not found');
+            return reject("Not found");
           }
         });
-      })
+      });
     },
 
-    eventsAdd: function (newItem) {
-      return new Promise(function (resolve, reject) {
-
+    eventsAdd: function(newItem) {
+      return new Promise(function(resolve, reject) {
         Event.create(newItem, (error, item) => {
           if (error) {
             // Cannot add item
@@ -84,221 +78,213 @@ module.exports = function (mongoDBConnectionString) {
           //Added object will be returned
           return resolve(item);
         });
-      })
+      });
     },
 
-    eventsEdit: function (newItem) {
-      return new Promise(function (resolve, reject) {
-
-        Event.findByIdAndUpdate(newItem.eventId, newItem, { new: true }, (error, item) => {
-          if (error) {
-            // Cannot edit item
-            return reject(error.message);
+    eventsEdit: function(newItem) {
+      return new Promise(function(resolve, reject) {
+        Event.findByIdAndUpdate(
+          newItem.eventId,
+          newItem,
+          { new: true },
+          (error, item) => {
+            if (error) {
+              // Cannot edit item
+              return reject(error.message);
+            }
+            // Check for an item
+            if (item) {
+              // Edited object will be returned
+              return resolve(item);
+            } else {
+              return reject("Not found");
+            }
           }
-          // Check for an item
-          if (item) {
-            // Edited object will be returned
-            return resolve(item);
-          } else {
-            return reject('Not found');
-          }
-
-        });
-      })
+        );
+      });
     },
 
-
-    eventDelete: function (eventId) {
-      return new Promise(function (resolve, reject) {
-
-        Event.findByIdAndRemove(eventId, (error) => {
+    eventDelete: function(eventId) {
+      return new Promise(function(resolve, reject) {
+        Event.findByIdAndRemove(eventId, error => {
           if (error) {
             // Cannot delete item
             return reject(error.message);
           }
           // Return success, but don't leak info
           return resolve();
-        })
-      })
+        });
+      });
     },
 
-    
-/*********************************************************          USER         *********************************************************************/
-eventsGetAll: function () {
-    return new Promise(function (resolve, reject) {
+    /*********************************************************          USER         *********************************************************************/
+    eventsGetAll: function() {
+      return new Promise(function(resolve, reject) {
+        // Fetch all documents
+        Event.find()
+          .sort({ eventId: "asc" })
+          .exec((error, items) => {
+            if (error) {
+              // Query error
+              return reject(error.message);
+            }
+            // Found, a collection will be returned
+            return resolve(items);
+          });
+      });
+    },
 
-      // Fetch all documents
-      Event.find()
-        .sort({ eventId: 'asc' })
-        .exec((error, items) => {
+    eventsGetById: function(eventId) {
+      return new Promise(function(resolve, reject) {
+        // Find one specific document
+        Student.findById(studentId, (error, item) => {
           if (error) {
-            // Query error
+            // Find/match is not found
             return reject(error.message);
           }
-          // Found, a collection will be returned
-          return resolve(items);
+          // Check for an item
+          if (item) {
+            // Found, one object will be returned
+            return resolve(item);
+          } else {
+            return reject("Not found");
+          }
         });
-    })
-  },
-
-
-  eventsGetById: function (eventId) {
-    return new Promise(function (resolve, reject) {
-
-      // Find one specific document
-      Student.findById(studentId, (error, item) => {
-        if (error) {
-          // Find/match is not found
-          return reject(error.message);
-        }
-        // Check for an item
-        if (item) {
-          // Found, one object will be returned
-          return resolve(item);
-        } else {
-          return reject('Not found');
-        }
       });
-    })
-  },
+    },
 
-  studentAdd: function (newItem) {
-    return new Promise(function (resolve, reject) {
-
-      Student.create(newItem, (error, item) => {
-        if (error) {
-          // Cannot add item
-          return reject(error.message);
-        }
-        //Added object will be returned
-        return resolve(item);
-      });
-    })
-  },
-
-  studentEdit: function (newItem) {
-    return new Promise(function (resolve, reject) {
-
-      Student.findByIdAndUpdate(newItem._id, newItem, { new: true }, (error, item) => {
-        if (error) {
-          // Cannot edit item
-          return reject(error.message);
-        }
-        // Check for an item
-        if (item) {
-          // Edited object will be returned
-          return resolve(item);
-        } else {
-          return reject('Not found');
-        }
-
-      });
-    })
-  },
-
-
-  studentDelete: function (itemId) {
-    return new Promise(function (resolve, reject) {
-
-      Student.findByIdAndRemove(itemId, (error) => {
-        if (error) {
-          // Cannot delete item
-          return reject(error.message);
-        }
-        // Return success, but don't leak info
-        return resolve();
-      })
-    })
-  },
-
-
-/*******************************************************          COMPANY         *********************************************************************/
-eventsGetAll: function () {
-    return new Promise(function (resolve, reject) {
-
-      // Fetch all documents
-      Event.find()
-        .sort({ eventId: 'asc' })
-        .exec((error, items) => {
+    studentAdd: function(newItem) {
+      return new Promise(function(resolve, reject) {
+        Student.create(newItem, (error, item) => {
           if (error) {
-            // Query error
+            // Cannot add item
             return reject(error.message);
           }
-          // Found, a collection will be returned
-          return resolve(items);
+          //Added object will be returned
+          return resolve(item);
         });
-    })
-  },
+      });
+    },
 
+    studentEdit: function(newItem) {
+      return new Promise(function(resolve, reject) {
+        Student.findByIdAndUpdate(
+          newItem._id,
+          newItem,
+          { new: true },
+          (error, item) => {
+            if (error) {
+              // Cannot edit item
+              return reject(error.message);
+            }
+            // Check for an item
+            if (item) {
+              // Edited object will be returned
+              return resolve(item);
+            } else {
+              return reject("Not found");
+            }
+          }
+        );
+      });
+    },
 
-  eventsGetById: function (eventId) {
-    return new Promise(function (resolve, reject) {
+    studentDelete: function(itemId) {
+      return new Promise(function(resolve, reject) {
+        Student.findByIdAndRemove(itemId, error => {
+          if (error) {
+            // Cannot delete item
+            return reject(error.message);
+          }
+          // Return success, but don't leak info
+          return resolve();
+        });
+      });
+    },
 
-      // Find one specific document
-      Student.findById(studentId, (error, item) => {
-        if (error) {
-          // Find/match is not found
-          return reject(error.message);
-        }
-        // Check for an item
-        if (item) {
-          // Found, one object will be returned
+    /*******************************************************          COMPANY         *********************************************************************/
+    eventsGetAll: function() {
+      return new Promise(function(resolve, reject) {
+        // Fetch all documents
+        Event.find()
+          .sort({ eventId: "asc" })
+          .exec((error, items) => {
+            if (error) {
+              // Query error
+              return reject(error.message);
+            }
+            // Found, a collection will be returned
+            return resolve(items);
+          });
+      });
+    },
+
+    eventsGetById: function(eventId) {
+      return new Promise(function(resolve, reject) {
+        // Find one specific document
+        Student.findById(studentId, (error, item) => {
+          if (error) {
+            // Find/match is not found
+            return reject(error.message);
+          }
+          // Check for an item
+          if (item) {
+            // Found, one object will be returned
+            return resolve(item);
+          } else {
+            return reject("Not found");
+          }
+        });
+      });
+    },
+
+    studentAdd: function(newItem) {
+      return new Promise(function(resolve, reject) {
+        Student.create(newItem, (error, item) => {
+          if (error) {
+            // Cannot add item
+            return reject(error.message);
+          }
+          //Added object will be returned
           return resolve(item);
-        } else {
-          return reject('Not found');
-        }
+        });
       });
-    })
-  },
+    },
 
-  studentAdd: function (newItem) {
-    return new Promise(function (resolve, reject) {
-
-      Student.create(newItem, (error, item) => {
-        if (error) {
-          // Cannot add item
-          return reject(error.message);
-        }
-        //Added object will be returned
-        return resolve(item);
+    studentEdit: function(newItem) {
+      return new Promise(function(resolve, reject) {
+        Student.findByIdAndUpdate(
+          newItem._id,
+          newItem,
+          { new: true },
+          (error, item) => {
+            if (error) {
+              // Cannot edit item
+              return reject(error.message);
+            }
+            // Check for an item
+            if (item) {
+              // Edited object will be returned
+              return resolve(item);
+            } else {
+              return reject("Not found");
+            }
+          }
+        );
       });
-    })
-  },
+    },
 
-  studentEdit: function (newItem) {
-    return new Promise(function (resolve, reject) {
-
-      Student.findByIdAndUpdate(newItem._id, newItem, { new: true }, (error, item) => {
-        if (error) {
-          // Cannot edit item
-          return reject(error.message);
-        }
-        // Check for an item
-        if (item) {
-          // Edited object will be returned
-          return resolve(item);
-        } else {
-          return reject('Not found');
-        }
-
+    studentDelete: function(itemId) {
+      return new Promise(function(resolve, reject) {
+        Student.findByIdAndRemove(itemId, error => {
+          if (error) {
+            // Cannot delete item
+            return reject(error.message);
+          }
+          // Return success, but don't leak info
+          return resolve();
+        });
       });
-    })
-  },
-
-
-  studentDelete: function (itemId) {
-    return new Promise(function (resolve, reject) {
-
-      Student.findByIdAndRemove(itemId, (error) => {
-        if (error) {
-          // Cannot delete item
-          return reject(error.message);
-        }
-        // Return success, but don't leak info
-        return resolve();
-      })
-    })
-  },
-
     }
-}
+  };
+};
--- Web Services/server.js
+++ Web Services/server.js
@@ -1,7 +1,7 @@
 // Setup
 const express = require("express");
 const path = require("path");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 // Or use some other port number that you like better
@@ -12,14 +12,16 @@ const manager = require("./manager.js");
 //const m = manager("mongodb://localhost/coursedbweek2");
 // This one works for MongoDB Atlas...
 // Replace the database user name and password, and cluster name, with your own values
-const m = manager("mongodb://Kayaba:lmao@bti-as01-shard-00-00-gmim2.mongodb.net:27017,bti-as01-shard-00-01-gmim2.mongodb.net:27017,bti-as01-shard-00-02-gmim2.mongodb.net:27017/Assignment2?ssl=true&replicaSet=BTI-AS01-shard-0&authSource=admin&retryWrites=true");
+const m = manager(
+  "mongodb://Kayaba:lmao@bti-as01-shard-00-00-gmim2.mongodb.net:27017,bti-as01-shard-00-01-gmim2.mongodb.net:27017,bti-as01-shard-00-02-gmim2.mongodb.net:27017/Assignment2?ssl=true&replicaSet=BTI-AS01-shard-0&authSource=admin&retryWrites=true"
+);
 
 // Add support for incoming JSON entities
 app.use(bodyParser.json());
 
 // Deliver the app's home page to browser clients
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 // Resource not found (this should be at the end)
@@ -29,118 +31,138 @@ app.use((req, res) => {
 
 // Tell the app to start listening for requests
 app.listen(HTTP_PORT, () => {
-    console.log("Ready to handle requests on port " + HTTP_PORT);
+  console.log("Ready to handle requests on port " + HTTP_PORT);
 });
 
 /*******************************************************          EVENTS         *********************************************************************/
 
 // Get all
 app.get("/api/events", (req, res) => {
-    m.eventsGetAll()
-    .then((data) => {
+  m.eventsGetAll()
+    .then(data => {
       res.json(data);
     })
-    .catch((error) => {
-      res.status(500).json({ "message": error });
-    })
+    .catch(error => {
+      res.status(500).json({ message: error });
+    });
 });
 
 // Get one
 app.get("/api/events/:eventId", (req, res) => {
-    m.eventsGetById(req.params.eventId)
-    .then((data) => {
+  m.eventsGetById(req.params.eventId)
+    .then(data => {
       res.json(data);
     })
     .catch(() => {
-      res.status(404).json({ "message": "Resource not found" });
-    })
+      res.status(404).json({ message: "Resource not found" });
+    });
 });
 
 // Add new
 app.post("/api/events", (req, res) => {
-    m.eventsAdd(req.body)
-    .then((data) => {
+  m.eventsAdd(req.body)
+    .then(data => {
       res.json(data);
     })
-    .catch((error) => {
-      res.status(500).json({ "message": error });
-    })
+    .catch(error => {
+      res.status(500).json({ message: error });
+    });
 });
 
 // Edit existing
 app.put("/api/events/:eventId", (req, res) => {
-    m.eventsEdit(req.body)
-    .then((data) => {
+  m.eventsEdit(req.body)
+    .then(data => {
       res.json(data);
     })
     .catch(() => {
-      res.status(404).json({ "message": "Resource not found" });
-    })
+      res.status(404).json({ message: "Resource not found" });
+    });
 });
 
 // Delete item
 app.delete("/api/events/:eventId", (req, res) => {
-    m.eventsDelete(req.params.eventId)
+  m.eventsDelete(req.params.eventId)
     .then(() => {
       res.status(204).end();
     })
     .catch(() => {
-      res.status(404).json({ "message": "Resource not found" });
-    })
+      res.status(404).json({ message: "Resource not found" });
+    });
 });
 
 /*********************************************************          USER         *********************************************************************/
 
 // Get all
 app.get("/api/items", (req, res) => {
-    res.json({message: "fetch all items"});
+  res.json({ message: "fetch all items" });
 });
 
 // Get one
 app.get("/api/items/:itemId", (req, res) => {
-    res.json({message: "get user with Id: " + req.params.itemId});
+  res.json({ message: "get user with Id: " + req.params.itemId });
 });
 
 // Add new
 app.post("/api/items", (req, res) => {
-     res.json({message: "add a user item: " + req.body.firstName + " " + req.body.lastName});
+  res.json({
+    message: "add a user item: " + req.body.firstName + " " + req.body.lastName
+  });
 });
 
 // Edit existing
 app.put("/api/items/:itemId", (req, res) => {
-    res.json({message: "update user with Id: " + req.params.itemId + " to " + req.body.firstName + " " + req.body.lastName});
+  res.json({
+    message:
+      "update user with Id: " +
+      req.params.itemId +
+      " to " +
+      req.body.firstName +
+      " " +
+      req.body.lastName
+  });
 });
 
 // Delete item
 app.delete("/api/items/:itemId", (req, res) => {
-     res.json({message: "delete user with Id: " + req.params.itemId});
+  res.json({ message: "delete user with Id: " + req.params.itemId });
 });
 
 /*******************************************************          COMPANY         *********************************************************************/
 
 // Get all
 app.get("/api/items", (req, res) => {
-    res.json({message: "fetch all items"});
+  res.json({ message: "fetch all items" });
 });
 
 // Get one
 app.get("/api/items/:itemId", (req, res) => {
-    res.json({message: "get user with Id: " + req.params.itemId});
+  res.json({ message: "get user with Id: " + req.params.itemId });
 });
 
 // Add new
 app.post("/api/items", (req, res) => {
-     res.json({message: "add a user item: " + req.body.firstName + " " + req.body.lastName});
+  res.json({
+    message: "add a user item: " + req.body.firstName + " " + req.body.lastName
+  });
 });
 
 // Edit existing
 app.put("/api/items/:itemId", (req, res) => {
-    res.json({message: "update user with Id: " + req.params.itemId + " to " + req.body.firstName + " " + req.body.lastName});
+  res.json({
+    message:
+      "update user with Id: " +
+      req.params.itemId +
+      " to " +
+      req.body.firstName +
+      " " +
+      req.body.lastName
+  });
 });
 
 // Delete item
 app.delete("/api/items/:itemId", (req, res) => {
-     res.json({message: "delete user with Id: " + req.params.itemId});
+  res.json({ message: "delete user with Id: " + req.params.itemId });
 });
 
 /*****************************************************************************************************************************************************/
